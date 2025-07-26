68acec7aea81fbfefae93076acbba96ce105c3ed
Gabriel <igqribeiro@gmail.com>
2025-07-26 01:46:15 -0300

    novas ideias

--- elementos.js
+++ elementos.js
@@ -25,3 +25,28 @@ const TOTAL_ELEMENTOS = Array.from(
     ...Object.values(COMBINACOES),
   ])
 );
+
+/*
+
+"ide+codigo": "framework rural",
+"energia solar+automacao rural": "trator autonomo",
+"codigo+plataforma cooperativa": "app de cooperativa",
+"greve digital+assembleia": "votacao digital",
+"cafe+ide": "debugador acelerado",
+"terra+agroecologia": "horta permacultural",
+"cafe+hacktivismo": "criptocafe",
+"sustentabilidade+plataforma cooperativa": "mercado solidario",
+"sindicato+script de protesto": "campanha de boicote",
+"energia solar+plataforma cooperativa": "rede energetica popular",
+"framework rural+automacao rural": "infraestrutura digital no campo",
+"codigo+trator autonomo": "controle remoto agricola",
+"greve digital+criptocafe": "blockchain de resistência",
+"ide+votacao digital": "sistema de eleição participativa",
+"debugador acelerado+assembleia": "resolução de bugs coletivos",
+"sindicato+app de cooperativa": "central sindical digital",
+"horta permacultural+trator autonomo": "colheita regenerativa",
+"rede energetica popular+terra": "biodiversidade energizada",
+"criptocafe+terra": "plantacao descentralizada",
+"mercado solidario+codigo": "economia de código aberto"
+
+*/
